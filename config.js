d8f4185b5a9627677af59875c27d71f95fa178be
messibadronaldobetter <134239461+messibadronaldobetter@users.noreply.github.com>
2023-08-04 19:12:41 +1000

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+window.__CONFIG__ = {
+  // url must NOT end with a slash
+  VITE_CORS_PROXY_URL: "ibbimovies.xxhulusucksxx123.workers.dev",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkOTk4NTFkYWI1OTlmOTIzM2E0NTMyZWU2ZTgxOTAxYyIsInN1YiI6IjY0YmUyZWU2ZTlkYTY5MDBjYjdjYTYyNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Yu-SAQcXwfmx4N78orxw2Y8ZBSw3fQ_W4oGbKznDAiY"
+};
